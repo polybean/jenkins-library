b0cd12a4c2d4ace5f7161878c4f9fad2d7957327
polybean <polybean@gmail.com>
2019-09-23 20:59:30 +0800

    Refactor step names

--- vars/k8sFuncTestGolang.groovy
+++ vars/k8sFuncTestGolang.groovy
@@ -2,9 +2,9 @@ def call(project, domain, srcDirectory, useModule = true) {
     dir(srcDirectory) {
         addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
         if (useModule) {
-            sh "go get -d -v -t"
-        } else {
             sh "go mod download"
+        } else {
+            sh "go get -d -v -t"
         }
 
         sh "ADDRESS=${addr} go test ./... -v --run FunctionalTest"
